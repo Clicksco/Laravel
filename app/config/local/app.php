3389438daa5b92c5641e7e81c152acd0c1500c5b
Jon Park <jon.park@thap.co.uk>
2014-08-21 00:30:10 +0100

    Adding Jeffery Way's Generator package

--- app/config/local/app.php
+++ app/config/local/app.php
@@ -15,4 +15,21 @@ return array(
 
 	'debug' => true,
 
+
+    /*
+    |--------------------------------------------------------------------------
+    | Autoloaded Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | The service providers listed here will be automatically loaded on the
+    | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
+    |
+    | Additional Service Providers loaded only in development
+    |
+    */
+
+    'providers' => append_config([
+        'Way\Generators\GeneratorsServiceProvider',
+    ]),
 );
